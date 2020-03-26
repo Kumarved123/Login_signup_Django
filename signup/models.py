c59d2269917249978f64b1f8de51af9f9a2eb1fe
Kumarved123 <vkumar@ar.iitr.ac.in>
2020-03-26 22:14:21 +0530

    All Dome Ready to submit

--- signup/models.py
+++ signup/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 # Create your models here.
+class Details(models.Model):
+   ip_address = models.CharField(max_length = 50,  primary_key=True)
+    = models.DateTimeField(default=datetime.now()+timedelta(days=1))
+   time_in = models.DateTimeField(auto_now=True)
+   attemps = models.IntegerField()
 
+   def __str__(self):
+        return self.ip_address
--- signup/views.py
+++ signup/views.py
@@ -4,26 +4,73 @@ from django.shortcuts import render, redirect
 from signup.forms import RegistrationForm
 from django.contrib import messages
 from django.conf import settings
-from django.contrib.sessions.models import Session
+from . models import Details
+from datetime import datetime, timedelta
 
 import json
 import urllib
 
 # Create your views here.
 def register(request):
+    # getting the Ip address
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    # getting check whether this ip exist in the databse or not
+    try:
+        detail = Details.objects.get(pk=ip)
+    except :
+        detail = None
+    # if ip address exist
+    if detail:
+        expire = datetime.now()
+        curr = datetime.strptime(str(detail.time_out)[:19], "%Y-%m-%d %H:%M:%S")
+        
+        if curr<expire:
+            cap = False
+            detail.delete()
+            newip = Details.objects.create(ip_address= ip, attemps= 0, time_out= datetime.today() + timedelta(minutes=2))
+            newip.save()
+        else:
+            #check the number of attemps
+            if detail.attemps > 2:
+                cap = True
+                #dispaly the captcha
+            else:
+                cap = False
+                #not dispaly of captcha
+    # if ip address donot exist
+    else:
+        # not display of captcha
+        cap = False
+        newip = Details.objects.create(ip_address= ip, attemps= 0, time_out= datetime.today() + timedelta(minutes=2))
+        newip.save()
+
     if request.method =='POST':
+        if detail:
+            detail.attemps = detail.attemps + 1
+            detail.save()
+        else:
+            newip.attemps = newip.attemps + 1
+            newip.save()
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            recaptcha_response = request.POST.get('g-recaptcha-response')
-            url = 'https://www.google.com/recaptcha/api/siteverify'
-            values = {
-                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-                'response': recaptcha_response
-            }
-            data = urllib.parse.urlencode(values).encode()
-            req =  urllib.request.Request(url, data=data)
-            response = urllib.request.urlopen(req)
-            result = json.loads(response.read().decode())
+            if cap:
+                recaptcha_response = request.POST.get('g-recaptcha-response')
+                url = 'https://www.google.com/recaptcha/api/siteverify'
+                values = {
+                    'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                    'response': recaptcha_response
+                }
+                data = urllib.parse.urlencode(values).encode()
+                req =  urllib.request.Request(url, data=data)
+                response = urllib.request.urlopen(req)
+                result = json.loads(response.read().decode())
+            else:
+                result['success'] = True
             if result['success']:
                 form.save()
                 messages.success(request, 'New comment added with success!')
@@ -34,11 +81,13 @@ def register(request):
         else:
             messages.error(request,'Invalid Credentials, Try Again !!!!')
             return redirect('index')
-
     else:
+        if cap:
+            p  = "block"
+        else:
+            p = "none"
         form = RegistrationForm()
-
-        args = {'form': form }
+        args = {'form': form, 'cap':p}
         return render(request, 'index.html', args)
 def profile(request, pk=None):
     if not request.user.is_authenticated:
